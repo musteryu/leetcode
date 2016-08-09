87024ceb3eca132f676bc86ccfa1c721b2ab684e
musteryu <913811121@qq.com>
2016-08-09 22:20:05 +0800

    Leetcode

--- 282. Expression Add Operators/solution.cpp
+++ 282. Expression Add Operators/solution.cpp
@@ -1,4 +1,21 @@
 class Solution {
+private:
+    vector<string> res;
+    void dfs(const string& num, const int target, string cur, int pos, const long cv, const long pv, const char op) {
+        if (pos == num.size() && cv == target) {
+            res.push_back(cur);
+        } else {
+            for (int i=pos+1; i<=num.size(); i++) {
+                string t = num.substr(pos, i-pos);
+                long now = stol(t);
+                if (to_string(now) != t) continue;
+                dfs(res, num, target, cur+'+'+t, i, cv+now, now, '+');
+                dfs(res, num, target, cur+'-'+t, i, cv-now, now, '-');
+                dfs(res, num, target, cur+'*'+t, i, (op == '-') ? cv+pv - pv*now : ((op == '+') ? cv-pv + pv*now : pv*now), pv*now, op);
+            }
+        }
+    }
+
 public:
     vector<string> addOperators(string num, int target) {
         

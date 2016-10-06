94f9e7930b1386edf5de6db8fa766f4ba01905a2
musteryu <913811121@qq.com>
2016-10-06 14:53:08 -0700

    Oct 6 2016

--- 302. Smallest Rectangle Enclosing Black Pixels/Solution.java
+++ 302. Smallest Rectangle Enclosing Black Pixels/Solution.java
@@ -1,8 +1,31 @@
 public class Solution {
-    private int find(char[][] image, int l, int h) {
+    private boolean isWhite(char[][] image, int k, int mid, boolean rowWise) {
+        return (rowWise ? image[k][mid] : image[mid][k]) == '0';
+    }
+
+    private int bound(char[][] image, int i, int j, int low, int high, 
+                        boolean rowWise, boolean opt) {
+        while (i != j) {
+            int k = low;
+            int mid = (i + j) / 2;
+            while (k < high && isWhite(image, k, mid, rowWise)) 
+                ++k;
+            if (k < high == opt)
+                j = mid;
+            else
+                i = mid + 1;
+        }
+        return i;
         
     }
+
     public int minArea(char[][] image, int x, int y) {
-        
+        int rows = image.length;
+        int cols = image[0].length;
+        int rowLower = bound(image, 0, y, 0, rows, true, true);
+        int rowUpper = bound(image, y, cols, 0, rows, true, false);
+        int colLower = bound(image, 0, x, rowLower, rowUpper, false, true);
+        int colUpper = bound(image, x, rows, rowLower, rowUpper, false, false);
+        return (rowUpper - rowLower) * (colUpper - colLower);
     }
 }
